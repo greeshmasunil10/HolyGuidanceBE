fa52ea48a9dd94c4b9d2ad8526a00ed9d7c3a088
greeshmasunil10 <greeshmasunil10@gmail.com>
2023-02-14 18:31:09 -0500

    adding main.py

---
+++ main.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 13 11:40:52 2023
+
+@author: grees
+"""
+
+import os
+import openai
+from env import API_KEY, OPENAI_ORG
+openai.api_key = API_KEY
+openai.organization = OPENAI_ORG
+# openai.Model.list()
+
+
+
+# Define the prompt
+# prompt = "How do I deal with this problem?"
+
+# prompt = input("Enter your question: ")
+
+# helper = "I want you to act as a Bible expert and Pastor and provide guidance and support to people who seek help with their personal or spiritual issues. You will listen to their concerns and provide them with meaningful and biblical-based suggestions. Do not offer medical, legal or financial advice. Focus on offering wisdom and comfort from the scripture. Your responses must include a verson from the bible with explanation. Your response must also be to the point and compassionate. Your first message is: help me."
+
+# question = helper + prompt
+
+# Generate a response using GPT-3
+
+
+def ask_gpt(question):
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=question,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+    
+    return response["choices"][0]["text"]
+
+
+# # Print the response
+# print(ask_gpt(question))
+
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def ask_question():
+    question = request.args.get("question")
+    helper = "I want you to act as a Bible Expert and Pastor and provide guidance and support to people who seek help with their personal or spiritual issues. You will listen to their concerns and provide them with meaningful and biblical-based suggestions. Do not offer medical, legal or financial advice. Focus on offering wisdom and comfort from the scripture. Your responses must include a verse from the bible with an explanation. Your responses should not be lengthy or include a sermon, but rather be to the point and compassionate.  Your response should not contain the message. Your first message is: 'help me. "
+    question = helper + question + "'"
+    response = ask_gpt(question)
+    return jsonify({"response": response})
+
+if __name__ == "__main__":
+    app.run(debug=True)
