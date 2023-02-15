b604f19a32e28465e36b45faa93fba3341f70919
greeshmasunil10 <greeshmasunil10@gmail.com>
2023-02-14 19:52:24 -0500

    added deployment api url

--- src/App.js
+++ src/App.js
@@ -1,97 +1,3 @@
-// import React, { useState } from "react";
-// import "./App.css";
-
-// function App() {
-//   const [question, setQuestion] = useState("");
-//   const [response, setResponse] = useState("");
-//   const [error, setError] = useState(null);
-
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-//     setError(null);
-//     // Fetch the response from the API endpoint
-//     try {
-//       const res = await fetch(`http://localhost:5000/?question=${question}`);
-
-//       if (!res.ok) {
-//         throw new Error(`Error: ${res.status}`);
-//       }
-//       const data = await res.json();
-//       setResponse(data.response);
-//     } catch (err) {
-//       setError(err.message);
-//     }
-//   };
-
-//   return (
-//     <div className="content">
-//       <h1>HOLY GUIDANCE</h1>
-//       <form onSubmit={handleSubmit}>
-//         <label>
-//           Enter your question:
-//           <input
-//             type="text"
-//             value={question}
-//             onChange={(e) => setQuestion(e.target.value)}
-//           />
-//         </label>
-//         <button type="submit">Submit</button>
-//       </form>
-//       {error && <p style={{ color: "red" }}>{error}</p>}
-//       <div className="response">{response && <p>{response}</p>}</div>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-// import React, { useState } from "react";
-// import "./App.css";
-
-// function App() {
-//   const [question, setQuestion] = useState("");
-//   const [response, setResponse] = useState("");
-//   const [error, setError] = useState(null);
-
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-//     setError(null);
-//     // Fetch the response from the API endpoint
-//     try {
-//       const res = await fetch(`http://localhost:5000/?question=${question}`);
-
-//       if (!res.ok) {
-//         throw new Error(`Error: ${res.status}`);
-//       }
-//       const data = await res.json();
-//       setResponse(data.response);
-//     } catch (err) {
-//       setError(err.message);
-//     }
-//   };
-
-//   return (
-//     <div className="container">
-//       <form className="form" onSubmit={handleSubmit}>
-//         <label>
-//           Enter your question:
-//           <input
-//             type="text"
-//             value={question}
-//             onChange={(e) => setQuestion(e.target.value)}
-//           />
-//         </label>
-//         <button type="submit">Submit</button>
-//       </form>
-//       {error && <p className="error">{error}</p>}
-//       {response && <p>{response}</p>}
-//             </div>
-//           );
-//         }
-     
-        
-//         export default App;
-
 import React, { useState } from "react";
 import "./App.css";
 
@@ -104,7 +10,7 @@ function App() {
     e.preventDefault();
     setError(null);
     try {
-      const res = await fetch(`http://localhost:5000/?question=${question}`);
+      const res = await fetch(`https://holy-guidance-api.herokuapp.com/?question=${question}`);
       if (!res.ok) {
         throw new Error(`Error: ${res.status}`);
       }
