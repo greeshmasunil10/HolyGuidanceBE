0da307393b16cb8111c0d050e0957cd43acfc5c9
greeshmasunil10 <greeshmasunil10@gmail.com>
2023-02-14 19:37:37 -0500

    port binded

--- main.py
+++ main.py
@@ -53,3 +53,8 @@ def ask_question():
     question = helper + question + "'"
     response = ask_gpt(question)
     return jsonify({"response": response})
+
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
