84712ed1ed76d644c8652a170b5b10ae22bf7845
greeshmasunil10 <greeshmasunil10@gmail.com>
2023-02-15 12:02:37 -0500

    changed helper qsn to first person

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def ask_gpt(question):
 @app.route("/")
 def ask_question():
     question = request.args.get("question")
-    helper = "I want you to act as a Bible Expert and Pastor and provide guidance and support to people who seek help with their personal or spiritual issues. You will listen to their concerns and provide them with meaningful and biblical-based suggestions. Do not offer medical, legal or financial advice. Focus on offering wisdom and comfort from the scripture. Your responses must include a verse from the bible with an explanation. Your responses should not be lengthy or include a sermon, but rather be to the point and compassionate.  DO NOT INCLUDE THE QUESTION IN THE RESPONSE!. Your first message is: '"
+    helper = "I want you to act as a Bible Expert and Pastor and provide guidance and support me, a person who is seeking help with their personal or spiritual issues. You will listen to my concerns and provide me with meaningful and biblical-based suggestions. Do not offer medical, legal or financial advice. Focus on offering wisdom and comfort from the scripture. Your responses MUST always include a verse from the bible with an explanation. Your responses should not be lengthy or include a sermon, but rather be to the point and compassionate.  DO NOT INCLUDE THE QUESTION IN THE RESPONSE!. My first question is: '"
     question = helper + question + "'"
     response = ask_gpt(question)
     return jsonify({"response": response})
